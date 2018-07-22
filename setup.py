9b118173dd3cf833241c8e0f74ee901b5bbed1ef
Oleksandr Shyshatsky <oshyshatskyi@cloudlinux.com>
2018-07-22 09:57:26 +0300

    filex setup script

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 __author__ = "Aleksandr Shyshatsky"
 
-setup(name='World of Warships localization files downloader',
+setup(name='World of Warships files downloader',
       version='1.0',
-      description='Utility that downloads .mo files for specified languages and converts .mo to .po',
+      description='Utility that downloads game client files',
       author=__author__,
       author_email='shalal545@gmail.com',
-      url='https://github.com/Monstrofil/locale_exporter_wows',
+      url='https://github.com/Monstrofil/game_client_update_tool',
       scripts=['client_exporter.py'])
